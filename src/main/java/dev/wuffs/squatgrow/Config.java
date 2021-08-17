97073045c1c6fa1a17fe2c41bfcb4d95a9358a9f
Gareth Williams <gaz492@gmail.com>
2021-08-17 18:56:24 +0100

    Init

---
+++ src/main/java/dev/wuffs/squatgrow/Config.java
@@ -0,0 +1,51 @@
+package dev.wuffs.squatgrow;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.config.ModConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Config {
+    public static final String CATEGORY_GENERAL = "general";
+
+    public static ForgeConfigSpec CONFIG;
+
+    public static ForgeConfigSpec.BooleanValue opMode;
+    public static ForgeConfigSpec.BooleanValue useWhitelist;
+    public static ForgeConfigSpec.IntValue range;
+    public static ForgeConfigSpec.DoubleValue chance;
+    public static ForgeConfigSpec.ConfigValue<List<String>> blacklist;
+    public static ForgeConfigSpec.ConfigValue<List<String>> whitelist;
+
+
+    static {
+        ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+        List<String> defaultBlacklist = new ArrayList<>();
+        defaultBlacklist.add("minecraft:grass_block");
+        defaultBlacklist.add("minecraft:grass");
+        defaultBlacklist.add("minecraft:tall_grass");
+        defaultBlacklist.add("minecraft:netherrack");
+        defaultBlacklist.add("minecraft:warped_nylium");
+        defaultBlacklist.add("minecraft:crimson_nylium");
+
+        BUILDER.comment("General settings").push(CATEGORY_GENERAL);
+        opMode = BUILDER.comment("Enables OP mode!", "Enable at your own risk")
+                .define("opMode", false);
+        useWhitelist = BUILDER.comment("Use whitelist instead of blacklist")
+                .define("useWhitelist", false);
+        range = BUILDER.comment("Range for bonemeal effect")
+                .defineInRange("range", 3, 0, 16);
+        chance = BUILDER.comment("Chance of bonemeal effect", "0 being never and 1.0 being most of the time")
+                .defineInRange("chance", 0.5, 0, 1.0);
+        blacklist = BUILDER.comment("List of blocks to blacklist from twerking", "Tags can be used by using #minecraft:<tag_name> or #modid:<tag_name>")
+                        .define("blacklist", defaultBlacklist);
+        whitelist = BUILDER.comment("If useWhitelist is true use, only allow twerking the list below","Tags can be used by using #minecraft:<tag_name> or #modid:<tag_name>")
+                .define("whitelist", new ArrayList<>());
+
+        BUILDER.pop();
+
+        CONFIG = BUILDER.build();
+    }
+}
---
+++ src/main/java/dev/wuffs/squatgrow/common/CommonEvents.java
@@ -0,0 +1,82 @@
+package dev.wuffs.squatgrow.common;
+
+import dev.wuffs.squatgrow.Config;
+import dev.wuffs.squatgrow.SquatGrow;
+import net.minecraft.block.Block;
+import net.minecraft.block.IGrowable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.BoneMealItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+public class CommonEvents {
+
+    private static Map<UUID, Boolean> playerSneaking = new HashMap<>();
+
+    public static void init(final FMLCommonSetupEvent event) {
+        MinecraftForge.EVENT_BUS.addListener(CommonEvents::playerTickEvent);
+    }
+
+    public static void playerTickEvent(TickEvent.PlayerTickEvent event) {
+        PlayerEntity player = event.player;
+
+        if (player.level.isClientSide) return;
+        if (Config.opMode.get() && player.isCrouching()) {
+            doBoneMeal(player);
+        } else {
+            if (playerSneaking.containsKey(player.getUUID())) {
+                if (player.isCrouching() && !playerSneaking.get(player.getUUID())) {
+                    playerSneaking.put(player.getUUID(), true);
+                    doBoneMeal(player);
+                } else if (playerSneaking.get(player.getUUID()) && !player.isCrouching()) {
+                    playerSneaking.put(player.getUUID(), false);
+                }
+            } else {
+                playerSneaking.put(player.getUUID(), false);
+            }
+        }
+
+
+    }
+
+    public static void doBoneMeal(PlayerEntity player) {
+        World level = player.level;
+        BlockPos pos = player.blockPosition();
+
+        for (int x = -Config.range.get(); x <= Config.range.get(); x++) {
+            for (int z = -Config.range.get(); z <= Config.range.get(); z++) {
+                for (int y = -1; y <= 1; y++){
+                    BlockPos blockPos = new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
+                    Block block = level.getBlockState(blockPos).getBlock();
+                    if (block instanceof IGrowable && SquatGrow.allowTwerk(block.getRegistryName().toString(), block.getTags())) {
+                        Random r = new Random();
+                        double randomValue = 0 + (1 - 0) * r.nextDouble();
+                        SquatGrow.getLogger().debug("Rand value:" + randomValue);
+                        if (Config.chance.get() >= randomValue){
+                            BoneMealItem.applyBonemeal(new ItemStack(Items.BONE_MEAL), level, blockPos, player);
+                            ((ServerWorld) level).sendParticles((ServerPlayerEntity) player, ParticleTypes.HAPPY_VILLAGER, false, blockPos.getX() + 0.05D, blockPos.getY() + 0.05D, blockPos.getZ(), 10, 0.5, 0.5, 0.5, 3);
+                            SquatGrow.getLogger().debug("====================================================");
+                            SquatGrow.getLogger().debug("Block: " + block.getRegistryName().toString());
+                            SquatGrow.getLogger().debug("Tags: " + block.getTags());
+                            SquatGrow.getLogger().debug("Pos: " + blockPos);
+                            SquatGrow.getLogger().debug("====================================================");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
