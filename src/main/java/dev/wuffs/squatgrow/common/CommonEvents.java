e4fad28dc5faf42b08b842620533502057d2169c
Gareth Williams <gaz492@gmail.com>
2021-09-04 13:07:21 +0100

    Support for growing sugarcane @MichaelHillcox

--- src/main/java/dev/wuffs/squatgrow/common/CommonEvents.java
+++ src/main/java/dev/wuffs/squatgrow/common/CommonEvents.java
@@ -4,6 +4,7 @@ import dev.wuffs.squatgrow.Config;
 import dev.wuffs.squatgrow.SquatGrow;
 import net.minecraft.block.Block;
 import net.minecraft.block.IGrowable;
+import net.minecraft.block.SugarCaneBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.BoneMealItem;
@@ -58,17 +59,21 @@ public class CommonEvents {
 
         for (int x = -Config.range.get(); x <= Config.range.get(); x++) {
             for (int z = -Config.range.get(); z <= Config.range.get(); z++) {
-                for (int y = -1; y <= 1; y++){
+                for (int y = -1; y <= 1; y++) {
                     BlockPos blockPos = new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
                     Block block = level.getBlockState(blockPos).getBlock();
-                    if (block instanceof IGrowable && SquatGrow.allowTwerk(block.getRegistryName().toString(), block.getTags())) {
+                    if ((block instanceof IGrowable || block instanceof SugarCaneBlock) && SquatGrow.allowTwerk(block.getRegistryName().toString(), block.getTags())) {
                         Random r = new Random();
                         double randomValue = 0 + (1 - 0) * r.nextDouble();
                         if (Config.debug.get()) {
                             SquatGrow.getLogger().debug("Rand value:" + randomValue);
                         }
-                        if (Config.chance.get() >= randomValue){
-                            BoneMealItem.applyBonemeal(new ItemStack(Items.BONE_MEAL), level, blockPos, player);
+                        if (Config.chance.get() >= randomValue) {
+                            if (block instanceof SugarCaneBlock) {
+                                block.randomTick(level.getBlockState(blockPos), ((ServerWorld) level), blockPos, level.random);
+                            } else {
+                                BoneMealItem.applyBonemeal(new ItemStack(Items.BONE_MEAL), level, blockPos, player);
+                            }
                             ((ServerWorld) level).sendParticles((ServerPlayerEntity) player, ParticleTypes.HAPPY_VILLAGER, false, blockPos.getX() + 0.05D, blockPos.getY() + 0.05D, blockPos.getZ(), 10, 0.5, 0.5, 0.5, 3);
                             if (Config.debug.get()) {
                                 SquatGrow.getLogger().debug("====================================================");
