f9ef6b5bc519d9adacfbe978bec5d61450657405
Michael Hillcox <michaelhillcox@gmail.com>
2023-10-26 13:51:37 +0100

    fix: no debug

--- common/src/main/java/dev/wuffs/squatgrow/mixins/PlayerMixin.java
+++ common/src/main/java/dev/wuffs/squatgrow/mixins/PlayerMixin.java
@@ -2,14 +2,12 @@ package dev.wuffs.squatgrow.mixins;
 
 import dev.wuffs.squatgrow.SquatAction;
 import net.minecraft.world.entity.player.Player;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Debug(export = true)
 @Mixin(Player.class)
 public class PlayerMixin {
     @Unique boolean squatgrow$wasCrouchingLastTick = false;
